696327266bbbbd3c13f042f4e312001eb03b9035
WilliamZhu <allwefantasy@gmail.com>
2024-10-23 13:22:01 +0800

    auto_coder_pre_210_chat_action.yml_53134d608fbc6b7ccf7721b96efc9818

--- frontend/src/components/RAGList.tsx
+++ frontend/src/components/RAGList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Table, Button, message, Card, Typography, Space, Tag, Tooltip, Modal } from 'antd';
 import { PoweroffOutlined, PauseCircleOutlined, SyncOutlined, DatabaseOutlined, FileOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
